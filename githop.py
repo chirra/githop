15bddd23f49fbb63de9d07723fd20ae1a589dec9
Oleg Kashenko <cherriod@gmail.com>
2015-05-15 00:01:32 +0600

    Beta 1.0

--- githop.py
+++ githop.py
@@ -1,17 +1,15 @@
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+config_file = ".githop"
+
+#####################################
 import urllib2
 import sys
 import json
 import datetime
 
-
-
-
-config_file = ".githop"
-
 params = {}
-#params = {"server": "api.github.com"}
 
 
 def add_param(name, value):
@@ -21,43 +19,49 @@ def add_param(name, value):
 def set_default_params():
     params["server"] = "api.github.com"
     params["since"] = (datetime.datetime.now() - datetime.timedelta(days=7)).isoformat()
-    params["state"] = "close"
+    params["state"] = "closed"
 
 
 def print_usage_message():
-    print "Usage: githop user repository [server=githubserver] [since=YYYY-MM-DDTHH:MM:SSZ] [state=open|close] " \
+    print "Usage: githop user repository [server=githubserver] [since=YYYY-MM-DDTHH:MM:SSZ] [state=open|closed|all] " \
               "[assignee=<username>] [labels=<label1>,<label2>,..,<labelN>] [milestone=<milestone>]"
 
 
 def get_params_from_command_prompt():
-    for arg in sys.argv[1:]:
+
         try:
-            hlp = arg.split('=')
-            add_param(hlp[0], hlp[1])
+            params["repouser"] = sys.argv[1]
+            params["reponame"] = sys.argv[2]
+            for arg in sys.argv[3:]:
+                hlp = arg.split('=')
+                add_param(hlp[0], hlp[1])
         except:
             pass
 
-    if not params["server"] or not params["repouser"] or not params["reponame"]:
-        print_usage_message()
-        sys.exit(1)
+
+def check_params():
+    for key in ("server", "repouser", "reponame"):
+        if not params.get(key):
+            print "Missing parameter: {0}".format(key)
+            print_usage_message()
+            sys.exit(1)
 
 
 def get_params_from_file():
-    f = open(config_file)
-    lines = f.readlines()
-    f.close()
+    try:
+        f = open(config_file)
+        lines = f.readlines()
+        f.close()
 
-    for line in lines:
-        try:
+        for line in lines:
             hlp = line.split('=')
             add_param(hlp[0].strip(), hlp[1].strip())
-        except:
-            pass
+    except:
+        pass
 
 
 def create_url():
     # Exit with error if mustHave params not present
-
     try:
         url = "https://{0}/repos/{1}/{2}/issues".format(params.pop("server"), params.pop("repouser"), params.pop("reponame"))
     except:
@@ -74,35 +78,44 @@ def create_url():
 def get_url(url):
     try:
         result = urllib2.urlopen(url).read()
+        pass
     except:
-        result = "Url error %s", url
+        print "Url error {0}".format(url)
+        sys.exit(1)
     return result
 
 
-def parse_url(response, *args):
-    parsed_json = json.loads(response)  # list, every element is dictionary
+def parse_json(json_pack):
+    parsed_json = json.loads(json_pack)  # list, every element is dictionary
 
     result = []
+
     for element in parsed_json:
-        result.append("id: " + str(element["id"]))
-        result.append("title: " + element["title"])
-        result.append("body: " + element["body"])
-        result.append("updated_at: " + element["updated_at"])
-        result.append("state: " + element["state"])
-        result.append("assignee: " + element["assignee"]["login"])
-        result.append("milestone: " + element["milestone"]["title"] + ' ' + element["milestone"]["description"])
+        if element.get("id"): result.append("id: " + str(element["id"]))
+        if element.get("title"): result.append("title: " + element["title"])
+        if element.get("body"): result.append("body: " + element["body"])
+        if element.get("updated_at"): result.append("updated_at: " + element["updated_at"])
+        if element.get("state"): result.append("state: " + element["state"])
+        if element.get("assignee"): result.append("assignee: " + element["assignee"]["login"])
+        if element.get("milestone"): result.append("milestone: " + element["milestone"]["title"] + ' ' + element["milestone"]["description"])
         result.append('\n')
+
     return result
 
 
-def out(output_list):
-    for item in output_list:
+def print_list(my_list):
+    for item in my_list:
         print item
 
+#############################################
 
-get_params_from_file()
-get_params_from_command_prompt()
+# Highest parameters priority overwrite lowest
+set_default_params()    # In first, parameters dictionary fill default parameters
+get_params_from_file()  # In second, from configuration file
+get_params_from_command_prompt()    # In third, from command prompt, it's highest priority
+
+check_params()
 url = create_url()
 response = get_url(url)
-output_list = parse_url(response)
-out(output_list)
+output_list = parse_json(response)
+print_list(output_list)
