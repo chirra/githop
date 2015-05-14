ba334c2b48d37b7a211f8831dc1d1ef2f6c7a757
Oleg Kashenko <cherriod@gmail.com>
2015-05-14 22:37:13 +0600

    First worked version

--- githop.py
+++ githop.py
@@ -3,26 +3,43 @@
 import urllib2
 import sys
 import json
+import datetime
 
 
-params = {"server": "api.github.com"}
+
+
 config_file = ".githop"
+
+params = {}
+#params = {"server": "api.github.com"}
 
 
 def add_param(name, value):
     params[name] = value
 
 
-def get_params_from_command_prompt():
-    if len(sys.argv) < 2:
-        print "Usage: githop user repositarium [server=githubserver] [since=YYYY-MM-DDTHH:MM:SSZ] [state=open|close] " \
+def set_default_params():
+    params["server"] = "api.github.com"
+    params["since"] = (datetime.datetime.now() - datetime.timedelta(days=7)).isoformat()
+    params["state"] = "close"
+
+
+def print_usage_message():
+    print "Usage: githop user repository [server=githubserver] [since=YYYY-MM-DDTHH:MM:SSZ] [state=open|close] " \
               "[assignee=<username>] [labels=<label1>,<label2>,..,<labelN>] [milestone=<milestone>]"
+
+
+def get_params_from_command_prompt():
     for arg in sys.argv[1:]:
         try:
             hlp = arg.split('=')
             add_param(hlp[0], hlp[1])
         except:
             pass
+
+    if not params["server"] or not params["repouser"] or not params["reponame"]:
+        print_usage_message()
+        sys.exit(1)
 
 
 def get_params_from_file():
@@ -39,14 +56,18 @@ def get_params_from_file():
 
 
 def create_url():
-    #Exit with error if mustHave params not present
-    url = "https://{0}/repos/{1}/{2}/issues".format(params.pop("server"), params.pop("repouser"), params.pop("reponame"))
+    # Exit with error if mustHave params not present
 
-    #Ignore if params not present
-    #for (key, value) in params.items():
-    par = '&'.join(["%s=%s" % (k, v) for k, v in params.items() if v])
-    if par:
-        url = url + '?' + par
+    try:
+        url = "https://{0}/repos/{1}/{2}/issues".format(params.pop("server"), params.pop("repouser"), params.pop("reponame"))
+    except:
+        print_usage_message()
+        sys.exit(1)
+
+    # Ignore if optional params not present
+    pars = '&'.join(["%s=%s" % (k, v) for k, v in params.items() if v])
+    if pars:
+        url = url + '?' + pars
     return url
 
 
@@ -63,12 +84,15 @@ def parse_url(response, *args):
 
     result = []
     for element in parsed_json:
-        for key, value in element.items():
-            if key in args:
-                result.append("{0}: {1}".format(key, value))
-            #result.append((["%s=%s" % (k, v) for k, v in params.items() if v]))
-        result += '\n'
-        return result
+        result.append("id: " + str(element["id"]))
+        result.append("title: " + element["title"])
+        result.append("body: " + element["body"])
+        result.append("updated_at: " + element["updated_at"])
+        result.append("state: " + element["state"])
+        result.append("assignee: " + element["assignee"]["login"])
+        result.append("milestone: " + element["milestone"]["title"] + ' ' + element["milestone"]["description"])
+        result.append('\n')
+    return result
 
 
 def out(output_list):
@@ -80,6 +104,5 @@ get_params_from_file()
 get_params_from_command_prompt()
 url = create_url()
 response = get_url(url)
-output_list = parse_url(response, "state", "milestone", "title", "body", "labels")
-
-pass
+output_list = parse_url(response)
+out(output_list)
